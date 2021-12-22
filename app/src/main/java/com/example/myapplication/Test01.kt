311ff258d247a25872c8a95d170d5f5211d10a45
Zundal <ajs15010120@gmail.com>
2021-12-22 18:36:08 +0900

    변경

---
+++ app/src/main/java/com/example/myapplication/Test01.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+fun main () {
+    helloWorld()
+    println(add(1,2))
+}
+
+fun helloWorld() : Unit {
+    return println("This is first Practice")
+}
+
+fun add(a : Int , b : Int) : Int {
+    return a + b
+}
